fba47933c0dc33f7bb645c2fe230dadea5448db4
zeff2421 <191776689+zeff2421@users.noreply.github.com>
2025-06-18 18:50:02 +0200

    Create a function that take an operator and two numbers

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b
 }
+
+function operate(oper, a, b) {
+    if (oper === '+') {
+        return add(a, b);
+    } else if (oper === '-') {
+        return subtract(a, b);
+    } else if (oper === '*') {
+        return multiply(a, b);
+    } else if (oper === '/') {
+        return divide(a, b);
+    }
+}
