76965e8fac20d6b81d680ba4d5d8cea92c7b32c5
zeff2421 <191776689+zeff2421@users.noreply.github.com>
2025-06-20 17:11:22 +0200

    Populate the display for the second operand

--- script.js
+++ script.js
@@ -34,19 +34,30 @@ function populateDisplay(key, display) {
     // Ensure the decimal point comes in once
     if (key === '.') {
         if (first_operand.includes('.')) return;
+        if (second_operand.includes('.')) return;
     }
 
     // Ensure maximum digits on screen are 13
-    if (first_operand.length === 13) return;
+    if (first_operand.length === 13 || second_operand.length === 13) return;
 
-    
-    if (display.textContent === '0') {
-        first_operand = key;
+    if (operator === null) {
+        if (display.textContent === '0') {
+            first_operand = key;
+        } else {
+            first_operand += key;
+        }
+        display.textContent = first_operand;
     } else {
-        first_operand += key;
+        if (display.textContent === '0') {
+            second_operand = key;
+        } else {
+            second_operand += key;
+        }
+        display.textContent = second_operand;
     }
-    display.textContent = first_operand;
 }
+
+
 
 let digitButtons = document.querySelectorAll(".number");
 let display = document.querySelector(".output");
@@ -55,6 +66,14 @@ digitButtons.forEach(button => {
     button.addEventListener('click', () => {
         let buttonClicked = button.textContent;
         populateDisplay(buttonClicked, display);
-        alert(`${buttonClicked} clicked`)
     })
-})
+});
+
+let operateButtons = document.querySelectorAll(".operator");
+operateButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        let clicked = button.value;
+        operator = clicked;
+
+    })
+});
